8a2a56d8f064af4b1c5039c9db66358916718e8d
Jerome Yao <jeromeyao.sh@gmail.com>
2017-02-17 15:57:42 +0800

    second commit

--- prime.py
+++ prime.py
@@ -1,7 +1,8 @@
 #Fliename: prime.py
 
 
-'''from math import sqrt #载入根号函数
+
+from math import sqrt #载入根号函数
 
 str1 = input("请输入一个2以上的整数，程序可以显示出你想要的2～X范围内的素数：") #接受输入量
 print(type(str1))
@@ -21,11 +22,8 @@ while j < int(str1): #判断过程量小于输入量
         i = i + 1
     if (i > k):
         print(j),
-    j += 1'''
+    j += 1
 
-from math import sqrt
-a = 1
-b = sqrt(1)
 
 
 
