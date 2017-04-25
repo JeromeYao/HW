1f1d86ceb2378fd1bb2e08746f3594de7c14f640
JeromeYao <jeromeyao.sh@gmail.com>
2017-04-25 10:37:56 +0800

    initial commit

---
+++ NOP3C/NOP3C_Ch1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Filename:Python3-cookbook.py
+
+'''
+This file is a notebook of python3-cookbook chinese version
+https://github.com/yidao620c/python3-cookbook
+'''
+
+__author__ = 'JeromeYao'
+
+print('1.1解压序列赋值给多个变量')
+
+'''
+Q:现在有一个包含N个元素的元组或者是序列，
+怎样将它里面的值解压后同时赋值给N个变量？
+'''
+
